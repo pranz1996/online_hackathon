0c0b557a439bc01434df4d0c37bff3eceea4ec40
Aniket Deshpande <aniketdeshpande1995@gmail.com>
2019-05-07 22:38:24 -0700

    AdminHome, Payment, Register for Hackathon by Aniket Deshpande

--- client/src/components/RouteFile.js
+++ client/src/components/RouteFile.js
@@ -1,42 +1,51 @@
-import React, { Component } from 'react'
-import { Route } from 'react-router-dom'
-import CreateHackathon from './Hackathon/CreateHackathon'
-import ViewHackathon from './Hackathon/ViewHackathon'
-import Login from './Login/Login';
-import Signup from './Login/Signup';
-import LandingPage from './Login/LandingPage';
-import UserProfile from './User/UserProfile';
-import SearchOrganization from './Organization/SearchOrganization';
-import CreateOrganization from './Organization/CreateOrganization';
-import MyOrganization from './Organization/MyOrganization';
-import SearchHackathon from './Hackathon/SearchHackathon';
-import MyHackathon from './Hackathon/MyHackathon';
-import JudgeHackathon from './Judge/JudgeHackathon';
-import ShowEmailSent from './Login/ShowEmailSent';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+//import CreateHackathon from './Hackathon/CreateHackathon'
+import ViewHackathon from "./Hackathon/ViewHackathon";
+import Login from "./Login/Login";
+import Signup from "./Login/Signup";
+import LandingPage from "./Login/LandingPage";
+import UserProfile from "./User/UserProfile";
+import SearchOrganization from "./Organization/SearchOrganization";
+import CreateOrganization from "./Organization/CreateOrganization";
+import MyOrganization from "./Organization/MyOrganization";
+import SearchHackathon from "./Hackathon/SearchHackathon";
+import MyHackathon from "./Hackathon/MyHackathon";
+import JudgeHackathon from "./Judge/JudgeHackathon";
+import ShowEmailSent from "./Login/ShowEmailSent";
+import AdminHome from "./Admin/AdminHome";
+import RegisterForHackathon from "./Admin/RegisterForHackathon";
+import Payment from "./Admin/Payment";
+import ConfirmEmail from "./Admin/ConfirmEmail";
+import ConfirmEmailFinal from "./Admin/ConfirmEmailFinal";
 
-require('dotenv').config();
+require("dotenv").config();
 class RouteFile extends Component {
-    render() {
-        return (
-            <div>
-                <Route exact path="/" component={LandingPage} />
-                <Route path="/createHackathon" component={CreateHackathon} />
-                <Route path="/hackathonDetails" component={ViewHackathon} />
-                <Route path="/login" component={Login} />
-                <Route path="/signup" component={Signup} />
-                <Route path="/landingpage" component={LandingPage} />
-                <Route path="/user" component={UserProfile} />
-                <Route path="/searchOrganization" component={SearchOrganization} />
-                <Route path="/createOrganization" component={CreateOrganization} />
-                <Route path="/myOrganization" component={MyOrganization} />
-                <Route path="/searchHackathon" component={SearchHackathon} />
-                <Route path="/myHackathon" component={MyHackathon} />
-                <Route path="/judgeHackathon" component={JudgeHackathon}/>
-                <Route path="/showEmailSent" component={ShowEmailSent}/>
-
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Route exact path="/" component={LandingPage} />
+        {/* <Route path="/createHackathon" component={CreateHackathon} /> */}
+        <Route path="/hackathonDetails" component={ViewHackathon} />
+        <Route path="/login" component={Login} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/landingpage" component={LandingPage} />
+        <Route path="/user" component={UserProfile} />
+        <Route path="/searchOrganization" component={SearchOrganization} />
+        <Route path="/createOrganization" component={CreateOrganization} />
+        <Route path="/myOrganization" component={MyOrganization} />
+        <Route path="/searchHackathon" component={SearchHackathon} />
+        <Route path="/myHackathon" component={MyHackathon} />
+        <Route path="/judgeHackathon" component={JudgeHackathon} />
+        <Route path="/showEmailSent" component={ShowEmailSent} />
+        <Route path="/adminHome" component={AdminHome} />
+        <Route path="/registerForHackathon" component={RegisterForHackathon} />
+        <Route path="/payment" component={Payment} />
+        <Route path="/confirmEmail" component={ConfirmEmail} />
+        <Route path="/confirmEmailFinal" component={ConfirmEmailFinal} />
+      </div>
+    );
+  }
 }
 
 export default RouteFile;
