7c8ee8c5b42e35b5f7491b0a5a794e706c12c403
Vrushali Jadhav <cmpe281-vrushali-jadhav>
2019-05-08 15:00:05 -0700

    correction

---
+++ server/online-hackathon-project/src/main/java/com/openHack/ui/controller/OrganizationDenyRequestController.java
@@ -0,0 +1,48 @@
+package com.openHack.ui.controller;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.openHack.io.entity.OrganizationEntity;
+import com.openHack.io.entity.UserEntity;
+import com.openHack.service.DenyRequestService;
+import com.openHack.service.JoinRequestService;
+import com.openHack.shared.dto.DenyRequestDto;
+import com.openHack.shared.dto.JoinRequestDto;
+import com.openHack.shared.dto.OrganizationDto;
+import com.openHack.shared.dto.UserDto;
+import com.openHack.ui.model.request.JoinRequestDetailsModel;
+import com.openHack.ui.model.response.OrganizationDetailsResponseModel;
+import com.openHack.ui.model.response.UserDetailsResponseModel;
+
+@RestController
+@RequestMapping("denyrequest")	// http://localhost:8080/joinrequest
+public class OrganizationDenyRequestController {	
+	@Autowired
+	DenyRequestService denyRequestService;
+	
+	// join request deny by any organization
+	@PostMapping("/deny") 
+	public String denyJoinRequest(@RequestBody JoinRequestDetailsModel denyJoinRequestDetailsModel) { 
+		// JoinRequestDetailsModel object: input request data
+		
+		DenyRequestDto denyRequestDto = new DenyRequestDto();
+		BeanUtils.copyProperties(denyJoinRequestDetailsModel, denyRequestDto);
+				
+		// Service method Call to accept a request 
+		denyRequestService.denyRequest(denyRequestDto);
+				
+		return "successfully denied request";
+	}
+}
+	
--- server/online-hackathon-project/src/main/java/com/openHack/ui/controller/OrganizationJoinRequestController.java
+++ server/online-hackathon-project/src/main/java/com/openHack/ui/controller/OrganizationJoinRequestController.java
@@ -15,9 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.openHack.io.entity.OrganizationEntity;
 import com.openHack.io.entity.UserEntity;
-import com.openHack.service.DenyRequestService;
 import com.openHack.service.JoinRequestService;
-import com.openHack.shared.dto.DenyRequestDto;
 import com.openHack.shared.dto.JoinRequestDto;
 import com.openHack.shared.dto.OrganizationDto;
 import com.openHack.shared.dto.UserDto;
@@ -26,23 +24,83 @@ import com.openHack.ui.model.response.OrganizationDetailsResponseModel;
 import com.openHack.ui.model.response.UserDetailsResponseModel;
 
 @RestController
-@RequestMapping("denyrequest")	// http://localhost:8080/joinrequest
-public class OrganizationJoinRequestController {	
+@RequestMapping("joinrequest")	// http://localhost:8080/joinrequest
+public class OrganizationJoinRequestController {
+	
 	@Autowired
-	DenyRequestService denyRequestService;
+	JoinRequestService joinRequestService;
+	
+	// user send join request to any organization
+	@PostMapping("/send")  
+	public String sendJoinRequest(@RequestBody JoinRequestDetailsModel sendJoinRequestDetailsModel) {
+		
+		// JoinRequestDetailsModel object: input request data
+		
+		JoinRequestDto joinRequestDto = new JoinRequestDto();
+		BeanUtils.copyProperties(sendJoinRequestDetailsModel, joinRequestDto);
+	
+		// Service method Call to create a request 
+		joinRequestService.createRequest(joinRequestDto);
+		
+		return "The Join request to organization sent successfully ... ";
+	}
+	
+	 // organization accept the join request of user
+	@PostMapping("/accept")
+	public String acceptJoinRequest(@RequestBody JoinRequestDetailsModel acceptJoinRequestDetailsModel) {
+		
+		// JoinRequestDetailsModel object: input request data
+		
+		JoinRequestDto joinRequestDto = new JoinRequestDto();
+		BeanUtils.copyProperties(acceptJoinRequestDetailsModel, joinRequestDto);
+		
+		// Service method Call to accept a request 
+		joinRequestService.acceptRequest(joinRequestDto);
+		
+		return "successfully accepted the join request";
+	}
+	
+	 // show all sent request of user to organization(s)
+	@GetMapping(path = "/sent/{id}")
+	public List<String> sentJoinRequest(@PathVariable long id) {
+		
+		// Service method Call to get All organization details 
+		List<OrganizationEntity> returnValue = joinRequestService.getOrganizations(id);
+		
+		// System.out.println(returnValue);
+		
+		// Temporary organizations' data
+		List<String> organizations = new ArrayList<>();
+		for(OrganizationEntity entity : returnValue) {
+			organizations.add(entity.getName());
+		}
+		
+		return organizations;
+	}
+	
+	// show all request from users to any organization
+	@GetMapping(path = "/getRequestsForMyOrganisation/{id}") 
+	public ArrayList<UserDetailsResponseModel> hasJoinRequest(@PathVariable long id) {
+		// Service method Call to get All user details 
+		ArrayList<UserDto> returnValue = joinRequestService.getUsers(id);
+		
+		ArrayList<UserDetailsResponseModel> listOfUsers = new ArrayList<UserDetailsResponseModel>();
+		UserDetailsResponseModel singleResponseModel;
+			
+		Iterator dtoIterator = returnValue.iterator(); 
+			
+		while(dtoIterator.hasNext())
+		{
+			singleResponseModel = new UserDetailsResponseModel();
+			BeanUtils.copyProperties(dtoIterator.next(), singleResponseModel);
+			listOfUsers.add(singleResponseModel);
+		}
+		return listOfUsers;
+	}
 	
 	// join request deny by any organization
 	@PostMapping("/deny") 
-	public String denyJoinRequest(@RequestBody JoinRequestDetailsModel denyJoinRequestDetailsModel) { 
-		// JoinRequestDetailsModel object: input request data
-		
-		DenyRequestDto denyRequestDto = new DenyRequestDto();
-		BeanUtils.copyProperties(denyJoinRequestDetailsModel, denyRequestDto);
-				
-		// Service method Call to accept a request 
-		denyRequestService.denyRequest(denyRequestDto);
-				
-		return "successfully denied request";
+	public String denyJoinRequest() { 
+		return "successfully denied, the join request";
 	}
 }
-	
