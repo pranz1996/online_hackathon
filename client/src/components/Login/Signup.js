957e0a76d7503e2f6a5f7ca01c16c69cbb9daee3
Pranjal Patel <patelpranzp25@gmail.com>
2019-05-08 07:38:13 -0700

    Minor Changes in Email verification Logic

--- client/src/components/Login/Signup.js
+++ client/src/components/Login/Signup.js
@@ -131,23 +131,6 @@ export default class Signup extends Component {
             //         console.log("error" + errorMessage);
             //     });
             // }
-
-            var email = this.state.email
-            var password = this.state.password
-            fire.auth().createUserWithEmailAndPassword(email, password)
-            .then(function () {
-                fire.auth().currentUser.sendEmailVerification().then(function () {
-                    console.log("email verification sent to " + email);
-                });
-            }).catch(function (error) {
-                // Handle Errors here.
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                window.alert(errorCode + " " + errorMessage);
-                console.log("error" + errorMessage);
-            });
-
-            this.props.history.push('/showEmailSent');
     }
 
     render() {
--- client/src/fire.js
+++ client/src/fire.js
@@ -1,17 +1,17 @@
-import firebase from 'firebase';
-import { expression } from '@babel/template';
+// import firebase from 'firebase';
+// import { expression } from '@babel/template';
 
-var firebaseConfig = {
-  apiKey: "AIzaSyCy2ORALc4jTx90qSa-0EUS6nOJQLCZZ_U",
-  authDomain: "openhack-5bca3.firebaseapp.com",
-  databaseURL: "https://openhack-5bca3.firebaseio.com",
-  projectId: "openhack-5bca3",
-  storageBucket: "openhack-5bca3.appspot.com",
-  messagingSenderId: "1048548423850",
-  appId: "1:1048548423850:web:f5ebcab1b41037d0"
-};
-// Initialize Firebase
-const fire = firebase.initializeApp(firebaseConfig);
+// var firebaseConfig = {
+//   apiKey: "AIzaSyCy2ORALc4jTx90qSa-0EUS6nOJQLCZZ_U",
+//   authDomain: "openhack-5bca3.firebaseapp.com",
+//   databaseURL: "https://openhack-5bca3.firebaseio.com",
+//   projectId: "openhack-5bca3",
+//   storageBucket: "openhack-5bca3.appspot.com",
+//   messagingSenderId: "1048548423850",
+//   appId: "1:1048548423850:web:f5ebcab1b41037d0"
+// };
+// // Initialize Firebase
+// const fire = firebase.initializeApp(firebaseConfig);
 
 
-export default fire;
+// export default fire;
