46d2b0b0fa0537f858212f681f7f1aea05e97783
Pranjal Patel <patelpranzp25@gmail.com>
2019-05-20 10:52:35 -0700

    Minor Updates

--- server/online-hackathon-project/src/main/java/com/openHack/shared/dto/HackathonDto.java
+++ server/online-hackathon-project/src/main/java/com/openHack/shared/dto/HackathonDto.java
@@ -15,14 +15,8 @@ public class HackathonDto {
 	private String maxTeamSize;
 	private String createdBy;
 	private String status;
-	public String getStatus() {
-		return status;
-	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
+	
 	private boolean hackatonWinner = false;
-	private String status;
 	private List<UserEntity> judges;
 	public long getId() {
 		return id;
