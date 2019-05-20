159a5e6727620f06e996419444e7cbe3f2b8bbb5
Aakash Alurkar <aakash.alurkar95@gmail.com>
2019-05-19 18:33:34 -0700

    email template body

--- server/online-hackathon-project/src/main/java/com/openHack/SendEmail.java
+++ server/online-hackathon-project/src/main/java/com/openHack/SendEmail.java
@@ -10,13 +10,18 @@ public class SendEmail {
 	  
 	  List<String> listofemails = new ArrayList<String>();
 	  listofemails.add("aakash.alurkar95@gmail.com");
-	  listofemails.add("aakash.alurkar@sjsu.edu");
+//	  listofemails.add("aakash.alurkar@sjsu.edu");
+	  
+	  String url = "http://localhost:3000/payment";
 	  
 	  for(String s : listofemails) {
 		    Email from = new Email("test@example.com");
-		    String subject = "email subject";
+		    String subject = "Invoice from OpenHack";
 		    Email to = new Email(s);
-		    Content content = new Content("text/plain", "email body");
+//		    Content content = new Content("text/plain", "email body");
+		    Content content = new Content();
+		    content.setType("text/html");
+		    content.setValue("<html><body><b>test</b><br><br><a href = "+url+">test link</a></body></html>");
 		    Mail mail = new Mail(from, subject, to, content);
 		    SendGrid sg = new SendGrid("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");
 		    Request request = new Request();
