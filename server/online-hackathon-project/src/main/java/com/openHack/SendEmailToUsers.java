ee20fbb438d1b8ae4cc59ac8196bcdf6d4aa028a
Vrushali Jadhav <42917770+vrushali-jadhav@users.noreply.github.com>
2019-05-20 22:15:51 -0700

    Add files via upload

---
+++ server/online-hackathon-project/src/main/java/com/openHack/SendEmailToUsers.java
@@ -0,0 +1,33 @@
+package com.openHack;
+
+import com.sendgrid.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SendEmailToUsers 
+{
+	public boolean sendMail(String email, String subject, String body) throws IOException
+	{
+		String url = "http://localhost:3000/results";
+		
+		Email from = new Email("results@hackathon.com");
+	    Email to = new Email(email);
+
+		Content content = new Content();
+		content.setType("text/html");
+		content.setValue("<html><body><b>"+body+" : "+"</b><a href = "+url+">here</a></body></html>");
+		Mail mail = new Mail(from, subject, to, content);
+		SendGrid sg = new SendGrid("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");
+		Request request = new Request();
+		try 
+		{
+		    request.setMethod(Method.POST);
+			request.setEndpoint("mail/send");
+			request.setBody(mail.build());
+			Response response = sg.api(request);
+			} catch (IOException ex) {throw ex;}
+		  
+		return false;
+	}
+}
