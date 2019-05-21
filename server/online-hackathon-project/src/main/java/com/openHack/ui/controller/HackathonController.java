b68fb2c5823f13f186d9c817235fb756ad5c2cd3
Vrushali Jadhav <42917770+vrushali-jadhav@users.noreply.github.com>
2019-05-20 22:17:54 -0700

    Update HackathonController.java

--- server/online-hackathon-project/src/main/java/com/openHack/ui/controller/HackathonController.java
+++ server/online-hackathon-project/src/main/java/com/openHack/ui/controller/HackathonController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.openHack.SendEmailToUsers;
 import com.openHack.io.entity.HackathonEntity;
 import com.openHack.service.HackathonService;
 import com.openHack.shared.dto.HackathonDto;
